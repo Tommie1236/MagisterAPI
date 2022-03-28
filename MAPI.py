02de149fffd241ab6caf1e3b7667f2ad53bade43
tommie1236 <timooosterom@gmail.com>
2022-03-28 21:47:54 +0200

    added some code to read the agenda

--- MAPI.py
+++ MAPI.py
@@ -4,19 +4,23 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-SCHOOL_LINK = "" #link to your magister
-STUDENT_NUMBER = "" #Student number
-USER_PASSWORD = "" #Magister password 
+#You will see some time.sleep() statements on multiple locations if you get an error like this:
+#"{"method":"css selector","selector":"[id="username"]"}" 
+#Try making the delay before the dirver function a little longer, a second can go a long way.
 
-#Uncomment the browser you want to use:
-driver = webdriver.Chrome() #Optional argument, if not specified will search path.
+SCHOOL_LINK = "" #Put your link to your magister here
+STUDENT_NUMBER = 00000 #Put your student number here
+USER_PASSWORD = "" #Putt your Magister password here
+
+#Uncomment the browser you want to use:     The argument is optional if not specified it will search for path.
+driver = webdriver.Chrome() 
 #driver = webdriver.Firefox()
 #driver = webdriver.Edge()
 #driver = webdriver.Safari()
 
 driver.get(SCHOOL_LINK)
 
-time.sleep(1) #decreases the chance of failing
+time.sleep(1) #decreases the chance of failing. 
 
 student = driver.find_element(By.ID, "username")
 student.send_keys(STUDENT_NUMBER)
@@ -33,6 +37,12 @@ print(f"succesfully logged into {SCHOOL_LINK} using {str(STUDENT_NUMBER)}, {USER
 time.sleep(5)
 
 driver.get(SCHOOL_LINK+"/magister/#/agenda")
+
+time.sleep(10)
+
+agenda = driver.find_element(By.XPATH, '//*[@id="afsprakenLijst"]/div[2]/table/tbody/tr[2]/td[3]/span/span[2]')
+
+print(agenda.text)
 
 x = input("quit? ")
 x.lower()
